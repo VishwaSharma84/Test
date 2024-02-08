cbf11a26c3f4276eb7ec0e4b71b2b5cefd13b47e
VishwaSharma84 <127947793+VishwaSharma84@users.noreply.github.com>
2024-02-08 12:39:17 +0530

    feature engineering

--- feature_engineering.py
+++ feature_engineering.py
@@ -3,6 +3,7 @@ from data_preprocessing import data_preprocessing
 def feature_engineering():
   data = data_preprocessing()
   data.to_csv('loan_data_cleansed.csv', index=False)
-    return data
+  
+  return data
 
 feature_engineering()    
